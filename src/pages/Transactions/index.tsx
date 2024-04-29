5581b5f66f580c5c3257eb25c7ef3b6670bf4769
Democrito <demohenry6@gmail.com>
2024-04-29 16:05:08 -0300

    Criação da tabela de transações

--- src/pages/Transactions/index.tsx
+++ src/pages/Transactions/index.tsx
@@ -1,11 +1,39 @@
 import { Header } from '../../components/Header';
 import { Summary } from '../../components/Summary';
+import {
+	TransactionsContainer,
+	TransactionsTable,
+	PriceHighlight,
+} from './styles';
 
 export function Transactions() {
 	return (
 		<>
 			<Header />
 			<Summary />
+
+			<TransactionsContainer>
+				<TransactionsTable>
+					<tbody>
+						<tr>
+							<td>Desenvolvimento do site</td>
+							<td>
+								<PriceHighlight variant='income'>R$ 12.000.00</PriceHighlight>
+							</td>
+							<td>Venda</td>
+							<td>13/04/2022</td>
+						</tr>
+						<tr>
+							<td>Hamburguer</td>
+							<td>
+								<PriceHighlight variant='outcome'>-R$ 150,00</PriceHighlight>
+							</td>
+							<td>Alimentação</td>
+							<td>03/04/2022</td>
+						</tr>
+					</tbody>
+				</TransactionsTable>
+			</TransactionsContainer>
 		</>
 	);
 }
--- src/pages/Transactions/styles.ts
+++ src/pages/Transactions/styles.ts
@@ -1,1 +1,44 @@
+import { css, styled } from 'styled-components';
 
+export const TransactionsContainer = styled.main`
+	width: 100%;
+	max-width: 1220px;
+	margin: 4rem auto 0;
+	padding: 0 1.5rem;
+`;
+
+export const TransactionsTable = styled.table`
+	${({ theme }) => css`
+		width: 100%;
+		border-collapse: separate;
+		border-spacing: 0 0.5rem;
+		/* margin-top: 1.5rem; */
+
+		td {
+			padding: 1.25rem 2rem;
+			background: ${theme['gray-700']};
+
+			&:first-child {
+				width: 50%;
+				border-top-left-radius: 6px;
+				border-bottom-left-radius: 6px;
+			}
+
+			&:last-child {
+				border-top-right-radius: 6px;
+				border-bottom-right-radius: 6px;
+			}
+		}
+	`}
+`;
+
+interface PriceHighlightProps {
+	variant: 'income' | 'outcome';
+}
+
+export const PriceHighlight = styled.span<PriceHighlightProps>`
+	color: ${(props) =>
+		props.variant === 'income'
+			? props.theme['green-300']
+			: props.theme['red-300']};
+`;
